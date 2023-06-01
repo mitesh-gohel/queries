7fc77c6e4c00c7c1a552df5cb7b29742f1c4fd3b
mitesh-gohel <miteshgohel73@yahoo.com>
2023-06-01 14:34:52 +0530

    Add query in t3

---
+++ t3/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"myheader.h"
+
+int main()
+{
+	printf("sum = %d\n", my_sum(5, 23));
+	return 0;
+}
+
+/*
+Command for compilation :
+ gcc main.c sum.c
+*/
---
+++ t3/myheader.h
@@ -0,0 +1,12 @@
+#ifndef SUM_DEF
+#define SUM_DEF
+
+int my_sum(int , int );
+
+#endif
+
+
+/*
+Query :
+	In most of the header files, Why do we use if conditional compilation instead of directly declaring function prototype ?
+*/
